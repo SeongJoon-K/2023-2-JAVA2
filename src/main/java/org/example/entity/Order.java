81a5caa59a8184d1fb5b8585e4a35f59a9f11e53
Seongjoon-K <jobcho6320@naver.com>
2023-11-27 18:59:15 +0900

    feat: 주문 엔티티 수정

--- src/main/java/org/example/entity/Order.java
+++ src/main/java/org/example/entity/Order.java
@@ -14,26 +14,108 @@ public class Order {
     private Long firstBookId;
 
     @Column(name = "first_book_quantity")
-    private Integer firstBookQuantity;
+    private int firstBookQuantity;
 
     @Column(name = "second_book_id")
     private Long secondBookId;
 
     @Column(name = "second_book_quantity")
-    private Integer secondBookQuantity;
+    private int secondBookQuantity;
 
     @Column(name = "third_book_id")
     private Long thirdBookId;
 
     @Column(name = "third_book_quantity")
-    private Integer thirdBookQuantity;
+    private int thirdBookQuantity;
 
     @Column(name = "fourth_book_id")
     private Long fourthBookId;
 
     @Column(name = "fourth_book_quantity")
-    private Integer fourthBookQuantity;
+    private int fourthBookQuantity;
 
     @Column(name = "total_price", nullable = false)
-    private Double totalPrice;
+    private int totalPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFirstBookId() {
+        return firstBookId;
+    }
+
+    public void setFirstBookId(Long firstBookId) {
+        this.firstBookId = firstBookId;
+    }
+
+    public int getFirstBookQuantity() {
+        return firstBookQuantity;
+    }
+
+    public void setFirstBookQuantity(int firstBookQuantity) {
+        this.firstBookQuantity = firstBookQuantity;
+    }
+
+    public Long getSecondBookId() {
+        return secondBookId;
+    }
+
+    public void setSecondBookId(Long secondBookId) {
+        this.secondBookId = secondBookId;
+    }
+
+    public int getSecondBookQuantity() {
+        return secondBookQuantity;
+    }
+
+    public void setSecondBookQuantity(int secondBookQuantity) {
+        this.secondBookQuantity = secondBookQuantity;
+    }
+
+    public Long getThirdBookId() {
+        return thirdBookId;
+    }
+
+    public void setThirdBookId(Long thirdBookId) {
+        this.thirdBookId = thirdBookId;
+    }
+
+    public int getThirdBookQuantity() {
+        return thirdBookQuantity;
+    }
+
+    public void setThirdBookQuantity(int thirdBookQuantity) {
+        this.thirdBookQuantity = thirdBookQuantity;
+    }
+
+    public Long getFourthBookId() {
+        return fourthBookId;
+    }
+
+    public void setFourthBookId(Long fourthBookId) {
+        this.fourthBookId = fourthBookId;
+    }
+
+    public int getFourthBookQuantity() {
+        return fourthBookQuantity;
+    }
+
+    public void setFourthBookQuantity(int fourthBookQuantity) {
+        this.fourthBookQuantity = fourthBookQuantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
+
+
